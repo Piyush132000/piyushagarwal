9e1ebeb808e270b835f5224bf64d4695bc3d30d2
piyush132000 <piyush132000@gmail.com>
2022-07-10 14:11:43 +0530

    new deployment

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
+  lastScrollTop = 0;
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
