c0ac67a4d1ce2be47d027fe3b680f0925910a0a1
piyush132000 <piyush132000@gmail.com>
2022-07-11 15:22:07 +0530

    new deployment

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@ang
 export class HeaderComponent{
 
   @ViewChild('drawer', { static: false })  drawer: ElementRef | undefined;
-  @Input() title :string ="Piyush Agarwal";
+  @Input() title :string ="Piyush Agrawal";
   drawerShow: boolean = false;
   drawerEnabled:boolean = false;
   constructor( private render: Renderer2, private el: ElementRef) { }
