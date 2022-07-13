528f5ff40f76303f2f32a4ab3525ceafc487ce21
piyush132000 <piyush132000@gmail.com>
2022-07-13 13:23:29 +0530

    new components created

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent {
 
-  images: any[] = [ { img:'../../assets/images/js.png', animation:'slid-up'
+  images: any[] = [ { img:'./assets/images/js.png', animation:'slid-up'
   },
   { img:'./assets/images/angular1.png', animation:'slide-down'
   },
   { img:'./assets/images/icons8-git-100.png', animation:'slid-up'
   },
-  { img:'../assets/images/icons8-typescript.png', animation:'slide-down'
-  },{ img:'../../assets/images/icons8-react-native-100.png',animation:'slid-up' }
+  { img:'./assets/images/icons8-typescript.png', animation:'slide-down'
+  },{ img:'./assets/images/icons8-react-native-100.png',animation:'slid-up' }
 
   ]
 
